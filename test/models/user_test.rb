9f19f05739a157d86d06d1cdedd8851032e33126
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-08-24 14:27:34 +0530

    Rubocop issue autofix

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(
-      first_name: 'Sam',
-      last_name: 'Smith',
-      email: 'sam@sample.com'
+      first_name: "Sam",
+      last_name: "Smith",
+      email: "sam@sample.com"
     )
   end
 
@@ -16,44 +16,44 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_user_invalid_without_first_name
-    @user.first_name = ''
+    @user.first_name = ""
     assert_not @user.valid?
     @user.save
     assert_equal ["First name can't be blank"],
-                 @user.errors.full_messages
+      @user.errors.full_messages
   end
 
   def test_user_invalid_without_last_name
-    @user.last_name = ''
+    @user.last_name = ""
     assert_not @user.valid?
     @user.save
     assert_equal ["Last name can't be blank"],
-                 @user.errors.full_messages
+      @user.errors.full_messages
   end
 
   def test_reject_first_name_of_invalid_length
-    @user.first_name = 'a' * 60
+    @user.first_name = "a" * 60
     assert @user.invalid?
     @user.save
-    assert_equal ['First name is too long (maximum is 50 characters)'],
-                 @user.errors.full_messages
+    assert_equal ["First name is too long (maximum is 50 characters)"],
+      @user.errors.full_messages
   end
 
   def test_reject_last_name_of_invalid_length
-    @user.last_name = 'a' * 60
+    @user.last_name = "a" * 60
     assert @user.invalid?
     @user.save
-    assert_equal ['Last name is too long (maximum is 50 characters)'],
-                 @user.errors.full_messages
+    assert_equal ["Last name is too long (maximum is 50 characters)"],
+      @user.errors.full_messages
   end
 
   def test_user_should_be_not_be_valid_and_saved_without_email
-    @user.email = ''
+    @user.email = ""
     assert_not @user.valid?
 
     @user.save
-    assert_equal ["Email can't be blank", 'Email is invalid'],
-                 @user.errors.full_messages
+    assert_equal ["Email can't be blank", "Email is invalid"],
+      @user.errors.full_messages
   end
 
   def test_user_should_not_be_valid_and_saved_if_email_not_unique
@@ -62,8 +62,8 @@ class UserTest < ActiveSupport::TestCase
     test_user = @user.dup
     assert_not test_user.valid?
 
-    assert_equal ['Email has already been taken'],
-                 test_user.errors.full_messages
+    assert_equal ["Email has already been taken"],
+      test_user.errors.full_messages
   end
 
   def test_reject_email_of_invalid_length
@@ -72,9 +72,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_email_saving_in_lower_case
-    @user.email = 'SAM@SAMPLE.COM'
+    @user.email = "SAM@SAMPLE.COM"
     @user.save!
-    assert_equal 'sam@sample.com', @user.email
+    assert_equal "sam@sample.com", @user.email
   end
 
   def test_validation_should_accept_valid_addresses
@@ -100,7 +100,7 @@ class UserTest < ActiveSupport::TestCase
   def test_user_create_with_email_different_case
     @user.save!
     assert_raises ActiveRecord::RecordNotUnique do
-      User.create!(first_name: 'John', last_name: 'Smith', email: 'SAM@sample.com')
+      User.create!(first_name: "John", last_name: "Smith", email: "SAM@sample.com")
     end
   end
 end
