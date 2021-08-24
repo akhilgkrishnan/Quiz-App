5cea61773bb63717bd0e6e914065bd3ce41ceb86
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-08-24 17:14:23 +0530

    Role attribute added to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  enum role: { standard: 0, administrator: 1 }, _default: :standard
+
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
                     uniqueness: true,
                     length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX }
+  validates :role, presence: true, inclusion: { in: roles.keys, message: "Invalid Role" }
   before_save :to_lowercase
 
   private
