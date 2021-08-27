98bcfd815cf9009fe32a9363113ef2588f20350c
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-08-27 23:09:39 +0530

    User logs in landing page added

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 import Container from "components/Container";
+import Button from "components/Button";
 
-const Dashboard = () => {
+const Dashboard = ({ history }) => {
   return (
     <Container>
-      <div>Welcome to Dashboard</div>
+      <div>
+        <div className="flex justify-end">
+          <Button buttonText="Add new quiz" iconClass="ri-add-line" />
+        </div>
+        <h1 className="text-xl text-center text-gray-500 flex grid-rows-5 justify-center self-center mt-20">
+          You have not created any quiz
+        </h1>
+      </div>
     </Container>
   );
 };
