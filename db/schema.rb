be0708cd1bf940264f662df1d4f2920208a62fd5
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-08-27 23:21:39 +0530

    Quiz model migration added

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_014520) do
+ActiveRecord::Schema.define(version: 2021_08_27_174301) do
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_quizzes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", limit: 50, null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_08_25_014520) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "quizzes", "users"
 end
