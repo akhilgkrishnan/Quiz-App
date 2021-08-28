b3fb34fa270d0127b26ec342fbe2145f74789d70
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-08-28 08:47:27 +0530

    Current user validation added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     if session[:user_id]
-      @current_user = User.find(session[:user_id])
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      render json: {
+        notice: "Invalid User"
+      }, status: :unauthorized
     end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  skip_before_action :set_current_user
+
   def index
     render
   end
