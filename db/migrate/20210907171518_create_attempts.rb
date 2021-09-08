e3c305745db06e435d890359d3416ca95e46595e
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-09-08 17:37:30 +0530

    pluralized incorrect answer field

--- db/migrate/20210907171518_create_attempts.rb
+++ db/migrate/20210907171518_create_attempts.rb
@@ -7,7 +7,7 @@ class CreateAttempts < ActiveRecord::Migration[6.1]
       t.references :user, null: false, foreign_key: true
       t.boolean :submitted, default: false
       t.integer :correct_answers, default: 0, null: false
-      t.integer :incorrect_answer, default: 0, null: false
+      t.integer :incorrect_answers, default: 0, null: false
 
       t.timestamps
     end
