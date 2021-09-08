a577e1488b267598c4b472ccb1febec9119c1765
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-09-08 09:43:47 +0530

    Slug  indexing moved to change block.

--- db/migrate/20210907104607_add_slug_to_quiz.rb
+++ db/migrate/20210907104607_add_slug_to_quiz.rb
@@ -3,6 +3,6 @@
 class AddSlugToQuiz < ActiveRecord::Migration[6.1]
   def change
     add_column :quizzes, :slug, :string
+    add_index :quizzes, :slug, unique: true
   end
-  add_index :quizzes, :slug, unique: true
 end
